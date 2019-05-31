6af3208c4e3b6077ab9d22dc45c09d4068ad8a9f
VitaliySolonets <Solonets.80@mail.ru>
2019-05-31 22:58:15 +0700

    1.3. JavaDoc. Code convention[#144416]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,8 +1,20 @@
 package ru.job4j.calculate;
 
+/**
+ *Calculate
+ *
+ *@autor Vitaly Solonets (solonets.80@mail.ru)
+ */
+
 public class Calculate {
 	
+	/**
+	 *Main
+	 *
+	 *@param args - args.
+	 */
 		public static void main(String[] args) {
+			
 			System.out.println("Hello World");
 		}
 }
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,7 @@
+/**
+*Package for calculate task.
+*@author Vitaly Solonets (solonets.80@mail.ru)
+*@version $1$
+*@since 31.05.2019
+*/
+package ru.job4j.calculate;
