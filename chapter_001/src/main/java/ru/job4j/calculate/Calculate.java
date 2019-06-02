fa49d8e04a7d6c96e201dd0ec9afc9fee783cf07
VitaliySolonets <Solonets.80@mail.ru>
2019-06-02 21:00:40 +0700

    2.6. Invalid target release 11 [#144369]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -9,23 +9,20 @@ package ru.job4j.calculate;
  */
 
 public class Calculate {
-	
 	/**
 	 *Main
 	 *
 	 *@param args - args.
 	 */
-		public static void main(String[] args) {
-			
-			System.out.println("Hello World");
-		}
-		
-		/**
-		*Method Echo.
-		*@param name Your name.
-		*@return Echo plus your name.
-		*/
-		public String echo(String name) {
-			return "Echo, echo, echo :" + name;
-		}
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+	/**
+	 *Method Echo.
+	 *@param name Your name.
+	 *@return Echo plus your name.
+	 */
+	public String echo(String name) {
+		return "Echo, echo, echo :" + name;
+	}
 }
