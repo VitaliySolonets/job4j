1e32fe6cb0707c91d27640adfff3a5e45f6f3009
VitaliySolonets <Solonets.80@mail.ru>
2019-06-06 22:00:21 +0700

    Добавление Javadoc.

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -10,19 +10,39 @@ package ru.job4j.calculator;
 
 public class Calculator {
 
-
+    /**
+     * Addition method.
+     *
+     * @param first, second.
+     * @return first + second.
+     */
     public double add(double first, double second) {
         return first + second;
     }
-
+    /**
+     * Subtraction method.
+     *
+     * @param first, second.
+     * @return first - second.
+     */
     public double subtruct(double first, double second) {
         return first - second;
     }
-
+    /**
+     * Division method.
+     *
+     * @param first, second.
+     * @return first / second.
+     */
     public double div(double first, double second) {
         return first / second;
     }
-
+    /**
+     * Multiplication method.
+     *
+     * @param first, second.
+     * @return first * second.
+     */
     public double multiple(double first, double second) {
         return first * second;
     }
