3620dfd4a3f6205d25f6171bc9033df0996aa5f0
VitaliySolonets <Solonets.80@mail.ru>
2019-06-09 00:52:51 +0700

    4.3. Вычисление площади треугольника[#144378]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -0,0 +1,55 @@
+package ru.job4j.condition;
+
+/**
+ * @author Vitaly Solonets (solonets.80@mail.ru).
+ * @version $1.0$
+ * @since 08.06.2019.
+ */
+public class Triangle {
+    /**
+     * Метод вычисления полупериметра по длинам сторон.
+     * <p>
+     * Формула.
+     * <p>
+     * (a + b + c) / 2
+     *
+     * @param a расстояние между точками a b
+     * @param b расстояние между точками a c
+     * @param c расстояние между точками b c
+     * @return полупериметр.
+     */
+    public double period(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
+
+    /**
+     * Метод проверяет можно ли построить треугольник с такими длинами сторон.
+     *
+     * @param a Длина от точки a до b.
+     * @param b Длина от точки a до c.
+     * @param c Длина от точки b до c.
+     * @return boolean
+     */
+    private boolean exist(double a, double b, double c) {
+        return a + b > c && a + c > b && b + c > a;
+    }
+
+    /**
+     * Метод должен вычислить площадь треугольника.
+     * Формула.
+     * √ p *(p-a) * (p-b) * (p-c)
+     *
+     * @return Вернуть площадь если треугольник существует или -1.
+     */
+    public double area(int x1, int y1, int x2, int y2, int x3, int y3) {
+        double rls = -1;
+        double a = new Point().distance(x1, y1, x2, y2);
+        double b = new Point().distance(x2, y2, x3, y3);
+        double c = new Point().distance(x1, y1, x3, y3);
+        double p = period(a, b, c);
+        if (this.exist(a, b, c)) {
+            return Math.sqrt((p * (p - a) * (p - b) * (p - c)));
+        }
+        return rls;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.experimental.categories.Categories;
+
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.*;
+
+public class TriangleTest {
+    @Test
+    public void whenAreaSetThreePointsThenTriangleArea() {
+        Triangle triangle = new Triangle();
+        double result = triangle.area(0, 0, 0, 2, 2, 0);
+        double expected = 2D;
+        assertThat(result, closeTo(expected, 0.1));
+    }
+}
