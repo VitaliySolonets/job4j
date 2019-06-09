fcb060e36cff6a298c922988b9909637db90951f
VitaliySolonets <Solonets.80@mail.ru>
2019-06-09 21:22:31 +0700

    5.1. Подсчет суммы чётных чисел в диапазоне[#144383]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+/**
+ * @author Vitaly Solonets (solonets.80@mail.ru).
+ * @version $1.0$
+ * @since 09.06.2019.
+ */
+public class Counter {
+    public int add(int start, int finish) {
+        int sum = 0;
+        for (int index = start; index <= finish; index++) {
+            if (index % 2 != 0) {
+                continue;
+            }
+            sum += index;
+        }
+        return sum;
+    }
+}
