239120a2b025aecddcfcd11aa8bf220980ca9a55
VitaliySolonets <Solonets.80@mail.ru>
2019-06-12 21:44:00 +0700

    Исправление ошибок

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -11,14 +11,11 @@ package ru.job4j.loop;
 public class Factorial {
     public int calc(int n) {
         int result = 1;
-        if (n == 0) {
-            return result;
-        } else {
-            for (int index = 1; index <= n; index++) {
-                result *= index;
-            }
-            return result;
+        for (int index = 1; index <= n; index++) {
+            result *= index;
         }
+        return result;
     }
 }
+
 
