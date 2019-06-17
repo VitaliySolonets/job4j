9e2164e0cd7b2558afa89cfa97b0eee30d08d3b1
VitaliySolonets <Solonets.80@mail.ru>
2019-06-17 21:51:20 +0700

    5.5. Рефакторинг кода.[#144380]Добавление тестов для Пирамиды

--- chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -2,6 +2,7 @@ package ru.job4j.loop;
 
 import org.junit.Test;
 
+
 import java.util.StringJoiner;
 
 import static org.hamcrest.Matchers.is;
@@ -42,6 +43,23 @@ public class PaintTest {
                                 .add("  ^^")
                                 .add(" ^^^")
                                 .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+
+    @Test
+    public void whenPyramidBuilt() {
+        Paint paint = new Paint();
+        String rst = paint.pyramid(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^   ")
+                                .add("  ^^^  ")
+                                .add(" ^^^^^ ")
+                                .add("^^^^^^^")
                                 .toString()
                 )
         );
