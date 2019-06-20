c4dc8f086502bb88b7c65592cc28f6fbc4cf983b
VitaliySolonets <Solonets.80@mail.ru>
2019-06-20 22:10:23 +0700

    6.1. Классический поиск перебором.[#144390]

---
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+/**
+ * @author Vitaly Solonets (solonets.80@mail.ru
+ * @version $1.0$
+ * @since 20.06.2019
+ */
+public class FindLoop {
+    public int indexOf(int[] data, int el) {
+        int rst = -1;
+        for (int index = 0; index != data.length - 1; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+}
