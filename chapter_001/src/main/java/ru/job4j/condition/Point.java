811791424164b6f911279b2805e3a7f3970794e3
VitaliySolonets <Solonets.80@mail.ru>
2019-08-18 20:41:54 +0700

    2. Рефакторинг - Площадь треугольника.[#144483]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -45,7 +45,7 @@ public class Point {
      * Метод расчета использующий объекты.
      */
     public double distance(Point that) {
-        return sqrt(pow(this.x - that.x, 2) + pow(this.x - that.y, 2));
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
     /**
--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -6,6 +6,16 @@ package ru.job4j.condition;
  * @since 08.06.2019.
  */
 public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
     /**
      * Метод вычисления полупериметра по длинам сторон.
      * <p>
@@ -41,11 +51,11 @@ public class Triangle {
      *
      * @return Вернуть площадь если треугольник существует или -1.
      */
-    public double area(int x1, int y1, int x2, int y2, int x3, int y3) {
+    public double area() {
         double rls = -1;
-        double a = new Point(x1, y1).distance(new Point(x2, y2));
-        double b = new Point(x2, y2).distance(new Point(x3, y3));
-        double c = new Point(x3, y3).distance(new Point(x1, y3));
+        double a = first.distance(second);
+        double b = second.distance(third);
+        double c = first.distance(third);
         double p = period(a, b, c);
         if (this.exist(a, b, c)) {
             return Math.sqrt((p * (p - a) * (p - b) * (p - c)));
